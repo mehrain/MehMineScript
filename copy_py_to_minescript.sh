4395889c3fee431abe30b9a33be4ac702f074729
Mehrain <marijnrovers@live.nl>
2025-06-14 00:44:40 +0200

    Updated location.

--- copy_py_to_minescript.sh
+++ copy_py_to_minescript.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Copy all Python files from /home/mehrain/workspace/gh/mms to the original minescript directory
 
-SRC="/home/mehrain/workspace/gh/mms"
+SRC="/home/mehrain/workspace/gh/mms/minescript"
 DST="/mnt/c/Users/Mehrain/curseforge/minecraft/Instances/Script Kid/minescript"
 
 find "$SRC" -type f -name '*.py' -exec cp --parents {} "$DST" \;
+
+echo "Python files copied from $SRC to $DST"
