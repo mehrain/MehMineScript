7888c450c515911416606b74286e02ec8f85e445
Mehrain <marijnrovers@live.nl>
2025-06-14 20:52:02 +0200

    spamcrouching added

--- minescript/farm.py
+++ minescript/farm.py
@@ -1,1 +1,14 @@
-#start with crouching
+import time
+import random
+from system.lib import minescript as m
+
+def spamcrouch():
+    """Spam crouch for 30 seconds."""
+    # print("Spamming crouch for 30 seconds...")
+    # for _ in range(30):
+    m.player_press_sneak(True)
+    time.sleep(0.031)
+    m.player_press_sneak(False)
+
+while True:
+    spamcrouch()
